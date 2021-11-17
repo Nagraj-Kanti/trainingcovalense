e7ce01a86f31e441f8dc47cc20b1adee8ee97eaa
nagraj.kanti <nagraj.kanti@NagrajKanti.covalensedigital.net>
2021-11-17 17:59:42 +0530

    Commit 7:Change in DepartmentService class and InvalidDepartmentExceptionException class.

--- SpringBootMVCDemo/src/main/java/com/cov/exception/InvalidDepartmentIdException.java
+++ SpringBootMVCDemo/src/main/java/com/cov/exception/InvalidDepartmentIdException.java
@@ -1,5 +1,27 @@
 package com.cov.exception;
 
-public class InvalidDepartmentIdException {
+public class InvalidDepartmentIdException extends Exception{
+
+	public InvalidDepartmentIdException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public InvalidDepartmentIdException(String message, Throwable cause) {
+		super(message, cause);
+		
+	}
+
+	public InvalidDepartmentIdException(String message) {
+		super(message);
+		
+	}
+
+	public InvalidDepartmentIdException(Throwable cause) {
+		super(cause);
+		
+	}
+	
+	
 
 }
