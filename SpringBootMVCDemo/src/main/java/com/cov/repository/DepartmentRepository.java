a8b86e2ebeede072bc8bb142a9eb20eaca161c31
nagraj.kanti <nagraj.kanti@NagrajKanti.covalensedigital.net>
2021-11-18 11:13:49 +0530

    Commit 9:HomeController class is created and linked to home.jsp file.

--- SpringBootMVCDemo/src/main/java/com/cov/repository/DepartmentRepository.java
+++ SpringBootMVCDemo/src/main/java/com/cov/repository/DepartmentRepository.java
@@ -1,9 +1,11 @@
 package com.cov.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.cov.beans.Department;
 
+@Repository
 public interface DepartmentRepository extends JpaRepository<Department, Integer> {
 
 }
